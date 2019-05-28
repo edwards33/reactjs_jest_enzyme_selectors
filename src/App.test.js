bbdb1bf0883739d4decdefc98fbfc307ee9b95d9
Edward <edylennyy@gmail.com>
2019-05-28 23:05:33 +0300

    initial

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({
+  adapter: new Adapter()
+});
+
+describe('<App />', () => {
+  const wrapper = shallow(<App />);
+  it('contains Header element',() => {
+    expect(wrapper.find('Header').exists()).toBe(true);
+  });
+});
